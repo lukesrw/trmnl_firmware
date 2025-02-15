82836e6338c5405c62fd2cc36beba689a7241885
lukesrw <lukesrw+github@gmail.com>
2025-02-15 22:20:00 +0000

    Clean of `submitLogToApi`
    Added new `API_LOG_URL` config to allow logs to be sent to a different URL

--- include/api-client/submit_log.h
+++ include/api-client/submit_log.h
@@ -1,10 +1,6 @@
-
-#include <WString.h>
-
-struct LogApiInput
-{
-  String api_key;
-  const char *log_buffer;
+struct LogApiInput {
+    String api_key;
+    const char* log_buffer;
 };
 
-bool submitLogToApi(LogApiInput &input, const char *api_url);
+bool submitLogToApi(LogApiInput& input);
--- src/api-client/submit_log.cpp
+++ src/api-client/submit_log.cpp
@@ -1,83 +1,41 @@
 #include "api-client/submit_log.h"
-#include <stdio.h>
-#include "trmnl_log.h"
-#include <WiFiClientSecure.h>
-#include <HTTPClient.h>
+#include "api-client/httpsBegin.h"
+#include "config.h"
 
-bool submitLogToApi(LogApiInput &input, const char *api_url)
-{
-  String payload = "{\"log\":{\"logs_array\":[" + String(input.log_buffer) + "]}}";
+bool submitLogToApi(LogApiInput& input) {
+    String url = API_LOG_URL + String("/log/");
+    bool isHttps = String(API_LOG_URL).startsWith("https://");
 
-  WiFiClientSecure *secureClient = new WiFiClientSecure;
-  WiFiClient *insecureClient = new WiFiClient;
+    WiFiClientSecure secureClient;
+    WiFiClient insecureClient;
+    WiFiClient client = isHttps ? secureClient : insecureClient;
 
-  secureClient->setInsecure();
+    /**
+     * Skip certificate verification
+     */
+    secureClient.setInsecure();
 
-  bool isHttps = true;
-  if (String(api_url).indexOf("https://") == -1)
-  {
-    isHttps = false;
-  }
-
-  // define client depending on the isHttps variable
-  WiFiClient *client = isHttps ? secureClient : insecureClient;
-
-  bool result = false;
-  if (client)
-  {
-    {
-      // Add a scoping block for HTTPClient https to make sure it is destroyed before WiFiClientSecure *client is
-      HTTPClient https;
-      Log_info("[HTTPS] begin /api/log ...");
-
-      char new_url[200];
-      strcpy(new_url, api_url);
-      strcat(new_url, "/api/log");
-
-      if (https.begin(*client, new_url))
-      { // HTTPS
-        Log_info("[HTTPS] POST...");
-
-        https.addHeader("Accept", "application/json");
-        https.addHeader("Access-Token", input.api_key);
-        https.addHeader("Content-Type", "application/json");
-        Log_info("Send log - %s", payload.c_str());
-        // start connection and send HTTP header
-        int httpCode = https.POST(payload);
-
-        // httpCode will be negative on error
-        if (httpCode > 0)
-        {
-          // HTTP header has been send and Server response header has been handled
-          Log_info("[HTTPS] POST... code: %d", httpCode);
-          // file found at server
-          if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY || httpCode == HTTP_CODE_NO_CONTENT)
-          {
-            Log_info("[HTTPS] POST OK");
-            result = true;
-          }
-        }
-        else
-        {
-          Log_error("[HTTPS] POST... failed, error: %d %s", httpCode, https.errorToString(httpCode).c_str());
-          result = false;
-        }
-
-        https.end();
-      }
-      else
-      {
-        Log_error("[HTTPS] Unable to connect");
-        result = false;
-      }
+    HTTPClient http;
+    if (!http.begin(client, url)) {
+        return false;
     }
 
-    delete client;
-  }
-  else
-  {
-    Log_error("[HTTPS] Unable to create client");
-    result = false;
-  }
-  return result;
+    const String payload = "{\"log\":{\"logs_array\":[" + String(input.log_buffer) + "]}}";
+    http.addHeader("Accept", "application/json");
+    http.addHeader("Access-Token", input.api_key);
+    http.addHeader("Content-Length", String(payload.length()));
+    http.addHeader("Content-Type", "application/json");
+
+    int httpCode = http.POST(payload);
+    http.end();
+
+    switch (httpCode) {
+        case HTTP_CODE_OK:
+        case HTTP_CODE_MOVED_PERMANENTLY:
+        case HTTP_CODE_NO_CONTENT:
+            return true;
+
+        default:
+            return false;
+    }
 }
